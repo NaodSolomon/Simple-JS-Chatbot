060fcf767e84d7e831001b9b537a14e0d02bffba
Naod Solomon <naodsolomon8911@gmail.com>
2025-08-26 21:53:27 +0300

    Just checked my knowledge of ternary operators and checked property was successfull and moving on to the next question

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// #2 Accept User Input & Type Conversion
+// Use prompt() to ask the user for their age. Convert the input into a number, then log whether they are old enough to vote (18+).
+
 // let personName = document.getElementById("nameTxtBox");
 // let age = document.getElementById("ageTxtBox");
 // let displayBtn = document.getElementById("displayBtn");
@@ -10,6 +13,9 @@
 // }
 
 // displayBtn.onclick = displayNameAge;
+
+// #3 Constants & Math Object
+// Define a constant PI using Math.PI. Then calculate the area of a circle with radius 5.
 
 // const PI = Math.PI;
 // let r = document.getElementById("radiusBox");
@@ -24,22 +30,46 @@
 
 // resultBTN.onclick = calcAreaOfCircle;
 
-let up = document.getElementById('upperlimit');
-let down = document.getElementById('lowerlimit');
-let rand = document.getElementById('randombtn');
-let displayrand = document.getElementById('randomdisplay');
+// let up = document.getElementById('upperlimit');
+// let down = document.getElementById('lowerlimit');
+// let rand = document.getElementById('randombtn');
+// let displayrand = document.getElementById('randomdisplay');
 
-function randomize(){
-    const upper = Number(up.value);
-    const downer = Number(down.value);
 
-    let result = Math.floor(Math.random() * (upper - downer)) + downer;
-    if (result > 50){
-        displayrand.textContent = ` You Won! Your random number is ${result}`;
-    }
-    else{
-        displayrand.textContent = ` You Lost! Your random number is ${result}`;
-    }
-}
+// #4 Random Number Generator & If Statements
+// Generate a random number between 1 and 10. If the number is greater than 5, log "You win!", otherwise log "Try again."
 
-rand.onclick = randomize;
+
+// function randomize(){
+//     const upper = Number(up.value);
+//     const downer = Number(down.value);
+
+//     let result = Math.floor(Math.random() * (upper - downer)) + downer;
+//     if (result > 50){
+//         displayrand.textContent = ` You Won! Your random number is ${result}`;
+//     }
+//     else{
+//         displayrand.textContent = ` You Lost! Your random number is ${result}`;
+//     }
+// }
+
+// rand.onclick = randomize;
+
+
+// #5 Checkbox & Ternary Operator
+// Suppose you have an HTML checkbox:
+
+// <input type="checkbox" id="subscribe">
+
+
+// In JS, use the checked property and a ternary operator to log "Subscribed" if checked, otherwise "Not Subscribed".
+// let check = document.getElementById('subscribe');
+// let checkBTN = document.getElementById('checkbtn');
+// let h3display= document.getElementById('displaycheck')
+
+// function isFamily(){
+//     let message = check.checked? `Yay you are family!!`: `Please subscribe`;
+//     h3display.textContent = message;
+// }
+
+// checkBTN.onclick = isFamily;
